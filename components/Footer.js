dc58902382e008fdeedc9efeddfc5ea06349d0b9
Jasiu MacbookAir <jan@kaletka.com.pl>
2022-07-27 15:31:57 +0200

    About and lagrdeCard finished

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Lorem</p>
+        <p>Ipsuem</p>
+        <p>Sample</p>
+        <p>Awesome</p>
+        <p>Text</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Lorem</p>
+        <p>Ipsuem</p>
+        <p>Sample</p>
+        <p>Awesome</p>
+        <p>Text</p>
+      </div>{" "}
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Lorem</p>
+        <p>Ipsuem</p>
+        <p>Sample</p>
+        <p>Awesome</p>
+        <p>Text</p>
+      </div>{" "}
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Lorem</p>
+        <p>Ipsuem</p>
+        <p>Sample</p>
+        <p>Awesome</p>
+        <p>Text</p>
+      </div>{" "}
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>Lorem</p>
+        <p>Ipsuem</p>
+        <p>Sample</p>
+        <p>Awesome</p>
+        <p>Text</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import Head from 'next/head';
-import Banner from '../components/Banner';
-import Header from '../components/Header';
-import LargeCard from '../components/LargeCard';
-import MediumCard from '../components/MediumCard';
-import SmallCard from '../components/SmallCard';
-import data from '../data/data.json';
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
+import data from "../data/data.json";
 
 // dodac props lub {exploreData}
 export default function Home() {
   return (
-    <div className=''>
+    <div className="">
       <Head>
         <title>Just Time</title>
-        <link rel='icon' href='/Logo.png' />
+        <link rel="icon" href="/Logo.png" />
       </Head>
       {/* Header */}
       <Header />
       {/* Banner */}
       <Banner />
-      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section>
-          <h2 className='text-4xl font-semibold pb-5'>Explore offers</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          <h2 className="text-4xl font-semibold pb-5">Explore offers</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {data?.map(({ id, img, distance, location }) => (
               <SmallCard
                 key={id}
@@ -33,19 +34,21 @@ export default function Home() {
           </div>
         </section>
         <section>
-          <h2 className='text-4xl font-semibold py-8'>Find your specialist</h2>
-          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3'>
-          {data?.map(({id, img, title}) =>(
-            <MediumCard key={id} img={img} title={title} />
+          <h2 className="text-4xl font-semibold py-8">Find your specialist</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3">
+            {data?.map(({ id, img, title }) => (
+              <MediumCard key={id} img={img} title={title} />
             ))}
-            </div>
+          </div>
         </section>
         <LargeCard
-        img="/Logo.png"
-        title="Sample Title"
-        decription="Awesome sample description"
-        buttonText="Get shit done"/>
+          img="/Logo.png"
+          title="Sample Title"
+          decription="Awesome sample description"
+          buttonText="Get shit done"
+        />
       </main>
+      <Footer />
     </div>
   );
 }
@@ -66,7 +69,6 @@ export default function Home() {
 // dodatkowa sekcja
 // footer
 
-
 // odpalic front strone ta z SYG / przerobic na landing JT
 // backend ogarnac w mongoDB
-// projekty stron wizualne 
+// projekty stron wizualne
