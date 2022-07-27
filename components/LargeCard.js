f5482aa4d55b1ffc897e7c67b1737c195c630070
Jasiu MacbookAir <jan@kaletka.com.pl>
2022-07-27 14:59:00 +0200

    Large card

---
+++ components/LargeCard.js
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const LargeCard = ({img, title, description, buttonText}) => {
+  return (
+    <section>
+      <div><Image/>
+</div>
+<div></div>
+    </section>
+  )
+}
+
+export default LargeCard
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 import data from '../data/data.json';
@@ -39,6 +40,11 @@ export default function Home() {
             ))}
             </div>
         </section>
+        <LargeCard
+        img="/Logo.png"
+        title="Sample Title"
+        decription="Awesome sample description"
+        buttonText="Get shit done"/>
       </main>
     </div>
   );
